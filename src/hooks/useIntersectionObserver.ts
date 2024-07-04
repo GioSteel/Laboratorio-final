6cb274bab7603bef92bcb7391dcfeea8bbeb991f
Giovanni Bahamondes <giovannibahamondes@gmail.com>
2024-07-03 23:49:50 -0400

    added vulnerabilities

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -3,6 +3,18 @@ import { Request, Response } from 'express';
 import { db } from './db';
 // import { buildThresholdList } from "src/utils";
 
+
+
+export const getUser = (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const query = `SELECT * FROM users WHERE id = ${userId}`; // Directly using user input in SQL query
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+};
+
+
 export default function useIntersectionObserver(
   ref: RefObject<HTMLElement>,
   options?: IntersectionObserverInit
@@ -33,30 +45,10 @@ export default function useIntersectionObserver(
     };
   }, []);
 
-const arr = [1, 2, 3];
-console.log(arr[5]); 
 
-
-
-const user_name = 'John Doe'; // snake_case
-const userAge = 30; // camelCase
+const user_name = 'John Doe'; 
+const userAge = 30; 
 
   return entry;
 }
 
-export const getUser = (req: Request, res: Response) => {
-  const userId = req.params.userId;
-  const query = `SELECT * FROM users WHERE id = ${userId}`; // Directly using user input in SQL query
-  db.query(query, (err , result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-};
-
-const calculateArea = (width: number, height: number): number => {
-  return width * height;
-};
-
-const computeArea = (width: number, height: number): number => {
-  return width * height;
-}; // Identical code in different functions
