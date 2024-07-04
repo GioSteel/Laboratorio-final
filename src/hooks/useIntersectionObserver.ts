cd57aaa4accb2b23beaf0225b81d6a75e01d43f7
Giovanni Bahamondes <giovannibahamondes@gmail.com>
2024-07-03 23:37:46 -0400

    added vulnerabilities

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,4 +1,6 @@
 import { RefObject, useEffect, useState } from "react";
+import { Request, Response } from 'express';
+import { db } from './db';
 // import { buildThresholdList } from "src/utils";
 
 export default function useIntersectionObserver(
@@ -31,5 +33,30 @@ export default function useIntersectionObserver(
     };
   }, []);
 
+const arr = [1, 2, 3];
+console.log(arr[5]); 
+
+
+
+const user_name = 'John Doe'; // snake_case
+const userAge = 30; // camelCase
+
   return entry;
 }
+
+export const getUser = (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const query = `SELECT * FROM users WHERE id = ${userId}`; // Directly using user input in SQL query
+  db.query(query, (err , result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+};
+
+const calculateArea = (width: number, height: number): number => {
+  return width * height;
+};
+
+const computeArea = (width: number, height: number): number => {
+  return width * height;
+}; // Identical code in different functions
