fc76edbf9a6e029f934d147435a356444018981d
Giovanni Bahamondes <giovannibahamondes@gmail.com>
2024-07-07 13:13:20 -0400

    solving the issue with env

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,5 +1,6 @@
 import { CustomGenre } from "src/types/Genre";
 
+
 export const API_ENDPOINT_URL = import.meta.env.VITE_APP_API_ENDPOINT_URL;
 export const TMDB_V3_API_KEY = import.meta.env.VITE_APP_TMDB_V3_API_KEY;
 
